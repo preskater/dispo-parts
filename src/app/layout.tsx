c0aa6ebd190b2a81d1ab422f0e4f8b04769cf006
Clément Antoine XAVIER <clement-antoine.xavier@epitech.eu>
2026-01-15 16:34:54 +0900

    Feat: Add GitHub formatter for Next.js lint jobs output

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,9 +35,8 @@ export default function RootLayout({
       >
         <CustomNavbar />
         <main className="min-h-screen">
-
+          {children}
         </main>
-        {children}
         <CustomFooter />
       </body>
     </html >
